05dbc0f98b71a159cddc41da4afa68ed7608f13a
Antti Veikkolainen <antti.veikkolainen00@outlook.com>
2023-06-19 22:20:11 +0300

    Added tests

--- Blog/DependencyInjection/Services/IBlogCache.cs
+++ Blog/DependencyInjection/Services/IBlogCache.cs
@@ -1,26 +1,25 @@
-﻿namespace Blog.DependencyInjection.Services
+﻿namespace Blog.DependencyInjection.Services;
+
+/// <summary>Stores all the available blogs</summary>
+public interface IBlogCache
 {
-    /// <summary>Stores all the available blogs</summary>
-    public interface IBlogCache
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        public IEnumerable<BlogDto> GetAllBlogs();
-        
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="blogName"></param>
-        /// <returns></returns>
-        BlogDto? GetBlog(string blogName);
-        
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="blogName"></param>
-        /// <param name="blog"></param>
-        void SetItem(string blogName, BlogDto blog);
-    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerable<BlogDto> GetAllBlogs();
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="blogName"></param>
+    /// <returns></returns>
+    BlogDto? GetBlog(string blogName);
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="blogName"></param>
+    /// <param name="blog"></param>
+    void SetItem(string blogName, BlogDto blog);
 }
---
+++ Extensions/ObservableCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Blog.Extensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ObservableCollectionExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sequence"></param>
+        /// <param name="start"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public static ObservableCollection<T> Range<T>(this ObservableCollection<T> sequence, int start, int count)        
+            => new ObservableCollection<T>(sequence.Skip(start).Take(count));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> list)
+            => new ObservableCollection<T>(list);
+    }
+}
